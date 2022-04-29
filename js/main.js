887791fb3b828fb5a9f7d9241e7f894caddd2dbc
crohnaro <viniciosalfa@hotmail.com>
2022-04-29 15:02:40 -0300

    Adicionando a iteração de check das tarefas

--- js/main.js
+++ js/main.js
@@ -6,20 +6,29 @@ const Main = {
     },
     //Evento responsavel por amarzenar os dados vindo do html em variaveis
     cacheSelectors: function(){
-        this.checkButtons = document.querySelectorAll('.check')
+        this.$checkButtons = document.querySelectorAll('.check')
     },
     //Evento responsavel por tratar todos eventos de click ou interação do teclado
     bindEvents: function(){
         const self = this
 
-        this.checkButtons.forEach(function(button){
+        this.$checkButtons.forEach(function(button){
             button.onclick = self.Events.checkButton_click
         })
     },
     //Atributos separados
     Events:{
-        checkButton_click: function(){
-            alert('ok')
+        checkButton_click: function(e){
+            const li = e.target.parentElement
+            const isDone = li.classList.contains('done')
+
+            if (!isDone){
+                return li.classList.add('done')
+            }    
+            
+            li.classList.remove('done')
+            
+            
         }
     }
 
