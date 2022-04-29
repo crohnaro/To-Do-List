fb50381e23ffb7a6edbe183cd593517eec835bd3
crohnaro <viniciosalfa@hotmail.com>
2022-04-29 16:05:49 -0300

    Finalizado estrutura js com remoção e adição de tarefas

--- js/main.js
+++ js/main.js
@@ -7,6 +7,9 @@ const Main = {
     //Evento responsavel por amarzenar os dados vindo do html em variaveis
     cacheSelectors: function(){
         this.$checkButtons = document.querySelectorAll('.check')
+        this.$inputTask = document.querySelector('#inputTask')
+        this.$list = document.querySelector('#list')
+        this.$removeButtons = document.querySelectorAll('.remove')
     },
     //Evento responsavel por tratar todos eventos de click ou interação do teclado
     bindEvents: function(){
@@ -15,6 +18,13 @@ const Main = {
         this.$checkButtons.forEach(function(button){
             button.onclick = self.Events.checkButton_click
         })
+
+        this.$inputTask.onkeypress = self.Events.inpuTask_keypress.bind(this)
+
+        this.$removeButtons.forEach(function(button){
+            button.onclick = self.Events.removeButton_click
+        })
+        
     },
     //Atributos separados
     Events:{
@@ -27,8 +37,34 @@ const Main = {
             }    
             
             li.classList.remove('done')
-            
-            
+        },
+        inpuTask_keypress: function(e){
+            const key = e.key
+            const value = e.target.value
+
+            if (key === 'Enter'){
+                this.$list.innerHTML += `
+                <li>
+                    <div class="check"></div>
+                        <label class="task">
+                            ${value}
+                        </label>
+                        <button class="remove"></button>
+                </li>
+                `
+                e.target.value = ''
+                this.cacheSelectors()
+                this.bindEvents()
+            }
+                
+        },
+        removeButton_click: function(e){
+            let li = e.target.parentElement
+            li.classList.add('removed')
+
+            setTimeout(function(){
+                li.classList.add('hidden')
+            },300)
         }
     }
 
